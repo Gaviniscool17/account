645c4f17842e9dba43b15823e3dac076aa52d732
Team 1038 <team1038@1038.org>
2023-10-09 18:41:08 -0400

    ppp

--- AccountManager.java
+++ AccountManager.java
@@ -1,3 +1,13 @@
 public class AccountManager {
-    
+    private static AccountManager intsetsne;
+    private AccountManager() {
+        
+    }
+    public static AccountManager getInstance() {
+        if(intsetsne == null) {
+            intsetsne = new AccountManager();
+        }
+        return intsetsne;
+    }
+
 }
